b199588c55f851ee717d496a983f8e09c68fd3ef
Nikhil Narayen <nnarayen@berkeley.edu>
2016-01-24 23:20:38 -0800

    Switching column order to match decision index for admins
    Summary:
    As stated in summary
    
    Test Plan: Pure front end change, switching columns

--- app/assets/javascripts/components/admins/pages/overview/overview_table.jsx
+++ app/assets/javascripts/components/admins/pages/overview/overview_table.jsx
@@ -1,6 +1,6 @@
 /* Table Headers */
 TABLE_HEADERS = [
-    "Applicant Name", "Year", "GPA", "Major", "PRES", "IVP", "PVP", "VPD", "EVP"
+    "Applicant Name", "Year", "GPA", "Major", "PRES", "IVP", "VPD", "PVP", "EVP"
 ];
 
 class OverviewTable extends AltComponent {
